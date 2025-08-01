97c672d74e600490511e7884c693d96956d2b9ab
nolan2022 <nolan2022>
2025-08-01 18:28:54 +0800

    添加监听文本

--- bot.py
+++ bot.py
@@ -1,9 +1,21 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import requests
 
-def convert(update, context):
-    update.message.reply_text("✅ 收到指令，正在查询...")
 
+def handle_text(update, context):
+    if update.message.text == "查询":
+        update.message.reply_text("你说了 查询")
+    elif update.message.text == "我要退款":
+        update.message.reply_text("你说了 退款")
+    elif update.message.text == "转单":
+        update.message.reply_text("你说了 转单")
+
+def convert(update, context):
+    #update.message.reply_text("✅ 收到指令，正在查询...")
+    if update.message:
+        update.message.reply_text("✅ 收到指令，正在转化...")
+    else:
+        print("⚠️ update.message 为 None，可能是通过非消息方式触发的")
     try:
         # 获取指令参数
         if len(context.args) == 0:
@@ -32,7 +44,8 @@ def main():
     updater = Updater("5849011897:AAEUpFVWwE4PKVJq1UXusNjiZL3IfhZmS8E", use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("convert", convert))  # 注册命令
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
+    #dp.add_handler(CommandHandler("convert", convert))  # 注册命令
 
     updater.start_polling()
     updater.idle()
