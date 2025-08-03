13ee137002e46ade4e14e7762cb48df70d8b86b3
nolan2022 <nolan2022>
2025-08-03 22:33:20 +0800

    添加图片识别功能

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from convert import convert_bp
 from refund import refund_bp
 from query import query_bp
 from querys import querys_bp
-from verify import verify_bp
+#from verify import verify_bp
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 设置最大请求体为 50MB
@@ -13,7 +13,7 @@ app.register_blueprint(convert_bp)
 app.register_blueprint(refund_bp)
 app.register_blueprint(query_bp)
 app.register_blueprint(querys_bp)
-app.register_blueprint(verify_bp)
+#app.register_blueprint(verify_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
